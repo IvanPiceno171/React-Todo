fcae760b823359ec41d1d1639c10b011d2ac183f
unknown <ivan.piceno171@gmail.com>
2023-08-03 15:35:27 -0700

    comp restart

--- Mod4/backend/server.js
+++ Mod4/backend/server.js
@@ -45,14 +45,14 @@ app.use(async (req, res, next)=>{
         console.log(Object.keys(req))
         req.db.connection.config.namedPlaceholders=true
     
-        await next()
-        // req.db.release();
+        await next() // moves request to next middleware/endpoint its headed for
+        req.db.release(); 
     }catch(err){
         console.log(err)
-        // if(req.db){
-        //     req.db.release()
-        //     throw err
-        // }
+        if(req.db){
+            req.db.release()
+            throw err
+        }
     }
 })
 
@@ -66,23 +66,82 @@ const todos = [
     }
 ]
 
-app.get('/', (req, res)=>{
-    return res.send("Hello World")
-})
 
-app.get('/todos', (req, res)=>{
+app.get('/', (req, res)=>{
     
     // return res.status(200).json({data : todos} )
     return res.status(200).json({ todos } )
 })
 
-
-app.post('/todos', async (req, res)=>{
+app.post('/', async (req, res)=>{
     console.log('post todos', req.body)
     try{
         const newTodo = req.body;
         await todos.push(newTodo)
-        return res.status(201).json({ todos });
+
+        const [todo] = await req.db.query(`
+            INSERT INTO todos (title, completed)
+            VALUES (:title, :completed);
+            `, {title: newTodo.title,
+                completed: newTodo.completed
+            });
+
+        // return res.status(201).json({ todos });
+        return res.status(201).json({ ...todos, todo });
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({ error: 'Internal Server Error' });
+
+    }
+})
+
+// app.put('/:id', async (req, res) => {
+//     const todoId = req.params.id;
+//     const { title, completed } = req.body;
+  
+//     try {
+//       await req.db.query(`
+//         UPDATE todos
+//         SET title = :title, completed = :completed
+//         WHERE id = :todoId
+//       `, {
+//         title,
+//         completed,
+//         todoId,
+//       });
+  
+//       // Check if any row was affected by the update operation
+//       const [updatedTodo] = await req.db.query('SELECT * FROM todos WHERE id = ?', [todoId]);
+//       if (updatedTodo.length === 0) {
+//         return res.status(404).json({ error: 'Todo not found' });
+//       }
+  
+//       return res.status(200).json(updatedTodo[0]);
+//     } catch (err) {
+//       console.log(err);
+//       return res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   });
+  
+  
+
+app.delete('/:id', async (req, res)=>{
+    const todoId = req.params.id; // Get the id from the URL parameter
+
+    try{
+
+        const [todo] = await req.db.query(`
+            DELETE FROM todos
+            WHERE id = :todoId
+            `,{
+               todoId
+            });
+
+            if (todo.affectedRows === 0) {
+                return res.status(404).json({ error: 'Todo not found' });
+              }
+
+        return res.status(201).json({ success: "Todo deleted"});
     }catch(err){
         console.log(err)
         return res.status(500).json({ error: 'Internal Server Error' });
